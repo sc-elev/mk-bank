ddf447dfc4c695eace37b60d5c6e0067ddc96b0c
John Doe <john.doe@nowhere.net>
2016-03-04 15:47:46 +0100

    Adding missing files...

---
+++ Bank/Controllers/BankControllerFactory.cs
@@ -0,0 +1,44 @@
+﻿using Bank.Database;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Bank.Controllers
+{
+
+    public class BankControllerFactory : DefaultControllerFactory
+    {
+        private Dictionary<string, Func<RequestContext, IController>>
+            controllers;
+
+
+        public BankControllerFactory(IBankDbContext repository)
+        {
+            controllers =
+                new Dictionary<string, Func<RequestContext, IController>>();
+            controllers["Home"] = controller => new HomeController(repository);
+        }
+
+
+        public override IController
+        CreateController(RequestContext requestContext, string controllerName)
+        {
+            if (!controllers.ContainsKey(controllerName)) return null;
+
+            return controllers[controllerName](requestContext);
+        }
+
+
+        public static IControllerFactory GetControllerFactory()
+        {
+            string typeName = ConfigurationManager.AppSettings["repository"];
+            var type = Type.GetType(typeName);
+            var repository = Activator.CreateInstance(type);
+            return new BankControllerFactory(repository as IBankDbContext);
+        }
+    }
+}
